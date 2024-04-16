ccc4e25f50d0c8255f0ddbede457f5e70c6e9c0f
EasyBitPay Admin <easybitpay@sepantatech.com>
2024-04-16 12:59:47 +0000

    build command

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR;
+
+git pull
+
+npm i --force
+
+npm run build
+
+if [ -d "dist" ]; then
+    echo "Build Successfull."
+
+    if [ -d "web.backup" ]; then
+        rm -rf web.backup
+    fi
+    mv web web.backup
+    mv dist web
+    cd web
+else
+    echo "Dist directory does not exist."
+    echo "Failed to build."
+fi
+
